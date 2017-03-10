ac971dd4b64d9b1729a781273e61763365bb4592
skiermw <mworkman12@mchsi.com>
2017-03-10 11:35:03 -0600

    added style main and Example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,19 @@
 var React = require('react');
-
+var {Link} = require('react-router')
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here a few example locations to try</p>
+      <ol>
+        <li>
+          <Link to='/?location=Columbia, MO'>Columbia, MO</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil MO</Link>
+        </li>
+      </ol>
     </div>
-
   )
 };
 
